210ed21bcad26694fe0b8796b00a892be7f725b4
ida <idasarahchow@gmail.com>
2015-09-12 11:00:57 -0400

    scores

---
+++ public/js/testjs.js
@@ -0,0 +1,11 @@
+function scoreload() {
+
+    var sc1, sc2, sc3, sc4, sc5, sc6 = "0";
+
+    document.getElementById('score1').innerHTML = sc1;
+    document.getElementById('score2').innerHTML = sc2;
+    document.getElementById('score3').innerHTML = sc3;
+    document.getElementById('score4').innerHTML = sc4;
+    document.getElementById('score5').innerHTML = sc5;
+    document.getElementById('score6').innerHTML = sc6;
+}
