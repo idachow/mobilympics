a9bd5c67ac92243c9bb9408e1bddf08ab24b1674
ida <idasarahchow@gmail.com>
2015-09-12 12:53:23 -0400

    styling lead score

--- public/js/testjs.js
+++ public/js/testjs.js
@@ -1,7 +1,5 @@
 
 function scoreLoad() {
-    console.log("hello world");
-    
     var sc1 = Math.floor(Math.random() * 100);
     var sc2 = Math.floor(Math.random() * 100);
     var sc3 = Math.floor(Math.random() * 100);
@@ -11,6 +9,7 @@ function scoreLoad() {
     
     
     var allscores = [sc1,sc2,sc3,sc4,sc5,sc6];
+    console.log(allscores);
     
     var maxval = Math.max.apply( Math, allscores );
     var maxindex = 0;
@@ -20,24 +19,21 @@ function scoreLoad() {
         if (maxval == allscores[i]) {
             maxindex = i + 1;
             console.log("YES");
+            break;
         }
     }
-    
     
     
     console.log( "max value ~~~~ " + maxval);
     console.log( "max ~~~ " + maxindex);
     
-    var winning = maxindex.toString;
+    document.getElementById('score1').innerHTML = allscores[0];
+    document.getElementById('score2').innerHTML = allscores[1];
+    document.getElementById('score3').innerHTML = allscores[2];
+    document.getElementById('score4').innerHTML = allscores[3];
+    document.getElementById('score5').innerHTML = allscores[4];
+    document.getElementById('score6').innerHTML = allscores[5];
     
-
-    document.getElementById('score1').innerHTML = sc1;
-    document.getElementById('score2').innerHTML = sc2;
-    document.getElementById('score3').innerHTML = sc3;
-    document.getElementById('score4').innerHTML = sc4;
-    document.getElementById('score5').innerHTML = sc5;
-    document.getElementById('score6').innerHTML = sc6;
-    
-    var winningstring = allscores[maxindex - 1] + " <P><P><i><sub>SCORE</sub><P> <sub>LEAD</sub></i>";
+    var winningstring = "<div id='scorelead'>" + allscores[maxindex-1] + "<P>SCORE<P>LEAD </div>";
     document.getElementById('score'+maxindex).innerHTML = winningstring;
 }
