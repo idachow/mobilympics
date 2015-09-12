e40d26c4096e4c8d7073f87db44563892e7371ff
ida <idasarahchow@gmail.com>
2015-09-12 11:21:03 -0400

    scores again

--- public/js/testjs.js
+++ public/js/testjs.js
@@ -1,6 +1,12 @@
-function scoreload() {
-
-    var sc1, sc2, sc3, sc4, sc5, sc6 = "0";
+function scoreLoad() {
+    console.log("hello world");
+    
+    var sc1="111";
+    var sc2="222";
+    var sc3="333";
+    var sc4="444";
+    var sc5="555";
+    var sc6="666";
 
     document.getElementById('score1').innerHTML = sc1;
     document.getElementById('score2').innerHTML = sc2;
